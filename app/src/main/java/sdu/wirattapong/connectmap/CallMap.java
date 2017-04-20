e8a36be7374e6e15025581343e5d1bee83c0e180
superjo <superjowtf@hotmail.com>
2017-04-20 10:06:33 +0700

    Design UI

---
+++ app/src/main/java/sdu/wirattapong/connectmap/CallMap.java
@@ -0,0 +1,16 @@
+package sdu.wirattapong.connectmap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CallMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_map);
+    } //onCreate Method
+
+
+
+} //Main Class
